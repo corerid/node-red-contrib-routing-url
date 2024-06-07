66722e31885893ae8c536c5142c66ab7246443b0
Corerid <corerid@gmail.com>
2024-06-07 20:01:17 +0700

    Update routing.js

--- routing.js
+++ routing.js
@@ -1,28 +1,39 @@
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
+    function URLRoutingNode(config) {
         RED.nodes.createNode(this,config);
-        const { pathToRegexp } = require('path-to-regexp');
+        const createMatcher = require('feather-route-matcher');
+                    
         var node = this;
         node.on('input', function(msg) {
             var conditions = config.conditions || [];
             var results = new Array(conditions.length).fill(undefined);
+            let isMatched = false;
 
             for (var index = 0; index < conditions.length; index++) {
                 var condition = conditions[index];
+                let routeMatcher = {};
+                routeMatcher[condition.path] = condition.method;
+                const matcher = createMatcher(routeMatcher)
+
                 if (condition.path.trim() === "") {
                     node.error("Condition text cannot be empty", msg);
                     return;
                 }
 
-                var regex = pathToRegexp(condition.path);
-                if (regex.test(msg.req.url) && msg.req.method.toUpperCase() === condition.method) {
+                if (matcher(msg.req.url) && msg.req.method.toUpperCase() === condition.method) {
+                    isMatched = true;
                     results[index] = msg;
                     break;
                 }
             }
 
+            if (!isMatched) {
+                node.error("No matched route condition", msg);
+                return;
+            }
+
             node.send(results);
         });
     }
-    RED.nodes.registerType("routing",LowerCaseNode);
+    RED.nodes.registerType("routing", URLRoutingNode);
 }
